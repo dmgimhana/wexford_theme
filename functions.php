e5d093b5ad38b98d5caaa36c545ec3457f0cb74f
dmgimhana <dmgimhana@gmail.com>
2025-11-13 13:12:27 +0000

    used wp_head func and created functions.php

---
+++ functions.php
@@ -0,0 +1,11 @@
+<!-- This is the file that we use to have a conversion with the wordpress system itself -->
+
+<?php 
+
+    function university_files() {   
+        wp_enqueue_style('university_main_styles', get_stylesheet_uri());
+    }
+
+// wp_enqueue_scripts is used to load css or js files
+    add_action("wp_enqueue_scripts","university_files");
+?>
--- header.php
+++ header.php
@@ -1,1 +1,10 @@
-<h1>THIS IS THE HEADER</h1>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <!-- wp_head allows php to be control of our head section -->
+   <?php wp_head(); ?>
+</head>
+<body>
+    <h1>Wexford University</h1>
+</body>
+</html>
