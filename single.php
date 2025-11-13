b970617784cbef2d8b4c9169d1bb858cb8585cb5
dmgimhana <dmgimhana@gmail.com>
2025-11-13 12:43:58 +0000

    created single.php and page.php

--- single.php
+++ single.php
@@ -3,16 +3,12 @@ while (have_posts()) {
   the_post(); ?>
 
   <h2>
-    <a href="<?php the_permalink() ?>">
      <?php the_title(); ?>
-    </a>
   </h2>
 
   <?php
     the_content()
   ?>
-
-  <hr>
 
   <?php
 }
